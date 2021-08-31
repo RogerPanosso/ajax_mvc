442a9ee101987cba12faac4a63c273ffc30b0f6a
Roger Panosso <rogerninopa@gmail.com>
2021-08-30 21:23:12 -0300

    Implementando perante camada de auxilio(Core) da estrutura MVC do projeto arquivo Core.php contendo class Core sendo responsavel por iniciar estrutura MVC perante aplicação

---
+++ App/Core/Core.php
@@ -0,0 +1,11 @@
+<?php
+  /*
+  * Definição da class Core(auxiliar) sendo responsavel por startar e iniciar funcionamento da estrutura
+  * MVC diante a URL amigavel perante a aplicação. Sendo responsavel por receber dados após dominio raiz
+  * da aplicação(url) como classController/metodo(action)/parametros para realizar as devidas requisições
+  * ao usuário final
+  */
+  namespace App\Core;
+
+  //...
+?>
